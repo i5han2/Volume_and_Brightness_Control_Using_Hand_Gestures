3a9c56fa24779f451d217ac895e73a8a3ca34243
ISHAN <ishan.deshpande222@gmail.com>
2021-06-24 01:45:41 +0530

    Added handedness feature in find position function

--- handLmModel.py
+++ handLmModel.py
@@ -1,10 +1,8 @@
 import cv2
 import mediapipe as mp
-import time
-
 
 class handDetector():
-    def __init__(self, mode=False, maxHands=2, detectionCon=0.5, trackCon=0.5):
+    def __init__(self, mode=False, maxHands=2, detectionCon=0.7, trackCon=0.5):
         self.mode = mode
         self.maxHands = maxHands
         self.detectionCon = detectionCon
@@ -18,8 +16,6 @@ class handDetector():
     def findHands(self, img, draw=True):
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         self.results = self.hands.process(imgRGB)
-        # print(results.multi_hand_landmarks)
-
         if self.results.multi_hand_landmarks:
             for handLms in self.results.multi_hand_landmarks:
                 if draw:
@@ -27,20 +23,26 @@ class handDetector():
                                                self.mpHands.HAND_CONNECTIONS)
         return img
 
-    def findPosition(self, img, handNo=0, draw=True):
+    def findPosition(self, img, draw=True):
+        rlmlist = []
 
-        lmList = []
         if self.results.multi_hand_landmarks:
-            myHand = self.results.multi_hand_landmarks[handNo]
-            for id, lm in enumerate(myHand.landmark):
-                # print(id, lm)
-                h, w, c = img.shape
-                cx, cy = int(lm.x * w), int(lm.y * h)
-                # print(id, cx, cy)
-                lmList.append([id, cx, cy])
-                if draw:
-                    cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+            if len(self.results.multi_hand_landmarks) == 2:
+                rlmlist.append('both')
+            elif len(self.results.multi_hand_landmarks) == 1:
+                rlmlist.append(self.results.multi_handedness[0].classification[0].label)
 
-        return lmList
+            for n in self.results.multi_hand_landmarks:
+                lmList = []
+                myHand = n
+                for id, lm in enumerate(myHand.landmark):
+                    h, w, c = img.shape
+                    cx, cy = int(lm.x * w), int(lm.y * h)
+                    lmList.append([id, cx, cy])
+                    if draw:
+                        cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+                rlmlist.append(lmList)
+
+        return rlmlist
 
 
